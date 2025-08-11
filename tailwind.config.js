f109d53d6ed5c1d015f5d6a8c94f49067be286f0
wydvoy <55352035+wydvoy@users.noreply.github.com>
2025-08-11 12:47:58 +0200

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ['"Source Code Pro"', '"Courier New"', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
